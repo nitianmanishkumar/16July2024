fa6e71ac7a91ea05f163f2669306f5f08cd1497a
avina <avina@DESKTOP-REMTP4B>
2024-07-16 13:17:23 +0530

    commit1

---
+++ GithubDemo/src/pkg1/Test1.java
@@ -0,0 +1,17 @@
+package pkg1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		int a=5;
+		
+		int b=10;
+		
+		int c=a+b;
+		
+		System.out.println("the add no is "+c);
+
+	}
+
+}
